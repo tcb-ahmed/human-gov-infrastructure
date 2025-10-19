3f5f809b52934ac0828659647055ffc2978aea6f
Software Dev 2 <dev2@humangov.com>
2025-10-19 14:59:06 +0000

    Added variable and provisioners to Terraform module aws_humangov_infrastructure/main.tf

--- terraform/modules/aws_humangov_infrastructure/main.tf
+++ terraform/modules/aws_humangov_infrastructure/main.tf
@@ -1,21 +1,21 @@
 resource "aws_security_group" "state_ec2_sg" {
-  name = "huangov-${var.state_name}-ec2-sg"
+  name        = "huangov-${var.state_name}-ec2-sg"
   description = "Allow traffic on ports 22 and 80"
 
-     ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
     from_port   = 5000
     to_port     = 5000
@@ -24,12 +24,12 @@ resource "aws_security_group" "state_ec2_sg" {
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = ["sg-06fffb790b2746488"]
   }
-  
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -37,28 +37,44 @@ resource "aws_security_group" "state_ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-   tags = {
-     "Name" = "humangov-${var.state_name}"
-   }
+  tags = {
+    "Name" = "humangov-${var.state_name}"
+  }
 }
 
 resource "aws_instance" "state_ec2" {
-  ami = "ami-007855ac798b5175e"
-  instance_type = "t2.micro"
-  key_name = "tcb"
-  vpc_security_group_ids = [ aws_security_group.state_ec2_sg.id ]
+  ami                    = "ami-007855ac798b5175e"
+  instance_type          = "t2.micro"
+  key_name               = "tcb"
+  vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]
 
   iam_instance_profile = aws_iam_instance_profile.s3_dynamodb_full_access_instance_profile.name
 
+  # Wait 30 seconds and add the instance's SSH key to known_hosts
+  provisioner "local-exec" {
+    command = "sleep 30; ssh-keyscan ${self.private_ip} >> ~/.ssh/known_hosts"
+  }
+
+  # Add instance info to the Ansible hosts file ( "/etc/ansible/hosts" Ansible Default Inventory Path )
+  provisioner "local-exec" {
+    command = "echo ${var.state_name} id=${self.id} ansible_host=${self.private_ip} ansible_user=ubuntu us_state=${var.state_name} aws_region=${var.region} aws_s3_bucket=${aws_s3_bucket.state_s3.bucket} aws_dynamodb_table=${aws_dynamodb_table.state_dynamodb.name} >> /etc/ansible/hosts"
+  }
+
+  # Remove instance info from the Ansible hosts file on destroy
+  provisioner "local-exec" {
+    command = "sed -i '/${self.id}/d' /etc/ansible/hosts"
+    when    = destroy
+  }
+
   tags = {
-     "Name" = "humangov-${var.state_name}"
-   }
+    "Name" = "humangov-${var.state_name}"
+  }
 }
 
 resource "aws_dynamodb_table" "state_dynamodb" {
-  name = "humangov-${var.state_name}-dynamodb"
+  name         = "humangov-${var.state_name}-dynamodb"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "id"
+  hash_key     = "id"
 
   attribute {
     name = "id"
@@ -66,14 +82,14 @@ resource "aws_dynamodb_table" "state_dynamodb" {
   }
 
   tags = {
-     "Name" = "humangov-${var.state_name}"
-   }
+    "Name" = "humangov-${var.state_name}"
+  }
 }
 
 resource "random_string" "bucket_suffix" {
-  length = 4
+  length  = 4
   special = false
-  upper = false
+  upper   = false
 }
 
 
@@ -81,8 +97,8 @@ resource "aws_s3_bucket" "state_s3" {
   bucket = "humangov-${var.state_name}-s3-${random_string.bucket_suffix.result}"
 
   tags = {
-     "Name" = "humangov-${var.state_name}"
-   }
+    "Name" = "humangov-${var.state_name}"
+  }
 }
 
 resource "aws_iam_role" "s3_dynamodb_full_access_role" {
@@ -110,7 +126,7 @@ resource "aws_iam_role" "s3_dynamodb_full_access_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "s3_full_access_role_policy_attachment" {
-  role = aws_iam_role.s3_dynamodb_full_access_role.name
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
@@ -127,5 +143,5 @@ resource "aws_iam_instance_profile" "s3_dynamodb_full_access_instance_profile" {
 
   tags = {
     Name = "humangov-${var.state_name}"
-  }  
+  }
 }
--- terraform/modules/aws_humangov_infrastructure/variables.tf
+++ terraform/modules/aws_humangov_infrastructure/variables.tf
@@ -1,3 +1,7 @@
-variable "state_name"{
-    description = "The name of the US state"
+variable "state_name" {
+  description = "The name of the US state"
+}
+
+variable "region" {
+  default = "us-east-1"
 }
